0decadfc1bb8572293b6f1d6a580496e86c510f6
loui161h <55078332+loui161h@users.noreply.github.com>
2020-03-17 10:22:07 +0100

    style og script elementer

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+window.addEventListener("load", sidenVises);
 
+function sidenVises() {
+    console.log("sidenVises");
+    /* Her har vi sat width til 500px fordi menuen skal gå helt ud til kanten på mobilsiden*/
+    function openNav() {
+        document.getElementById("mySidenav").style.width = "500px";
+    }
+
+    /* Her har vi sat width på side naven til 0 når den skal lukke igen*/
+    function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+    }
