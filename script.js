6bc761bf19f0331140a2c202521447084135c2dc
loui161h <55078332+loui161h@users.noreply.github.com>
2020-03-17 14:06:17 +0100

    rettelser i menuen

--- script.js
+++
@@ -1,9 +0,0 @@
-     /* Her har vi sat width til 500px fordi menuen skal gå helt ud til kanten på mobilsiden*/
-     function openNav() {
-         document.getElementById("mySidenav").style.width = "500px";
-     }
-
-     /* Her har vi sat width på side naven til 0 når den skal lukke igen*/
-     function closeNav() {
-         document.getElementById("mySidenav").style.width = "0";
-     }
