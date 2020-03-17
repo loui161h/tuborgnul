e4d29dde54d44192d9dc94b33aa5db4f2d216e3c
loui161h <55078332+loui161h@users.noreply.github.com>
2020-03-17 11:32:52 +0100

    nav

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-window.addEventListener("load", sidenVises);
+     /* Her har vi sat width til 500px fordi menuen skal gå helt ud til kanten på mobilsiden*/
+     function openNav() {
+         document.getElementById("mySidenav").style.width = "500px";
+     }
 
-function sidenVises() {
-    console.log("sidenVises");
-    /* Her har vi sat width til 500px fordi menuen skal gå helt ud til kanten på mobilsiden*/
-    function openNav() {
-        document.getElementById("mySidenav").style.width = "500px";
-    }
-
-    /* Her har vi sat width på side naven til 0 når den skal lukke igen*/
-    function closeNav() {
-        document.getElementById("mySidenav").style.width = "0";
-    }
+     /* Her har vi sat width på side naven til 0 når den skal lukke igen*/
+     function closeNav() {
+         document.getElementById("mySidenav").style.width = "0";
+     }
